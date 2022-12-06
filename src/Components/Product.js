4321fdbac69926da94ed14137f0471bed2f22a5f
SandeepPyata <pyatasandeepsandy@gmail.com>
2022-12-06 06:20:28 +0530

    modified Exercise-2
    This reverts commit 7d194c515062908dc5f654ab4eb6bb1ad8f2c299.

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import useProductData from "../hooks/useProductData";
 import { useParams } from "react-router-dom";
-function SingleProduct(props) {
-  const [imageLink, setImageLink] = useState(props.details.image);
-  function handleDetails(variant) {
-    if (variant === "Grey") {
-      setImageLink((prevDetails) => (prevDetails = variant.image));
-    } else {
-      setImageLink((prevDetails) => (prevDetails = variant.image));
-    }
-  }
 
-  const quantity = props.details.quantity;
+function SingleProduct(props) {
+  console.log(props);
   return (
     <>
-      <img src={imageLink} alt="Single-Product" />
+      <img src={props.details.image} alt="Single-Product" />
       <div id="single-product-description">
         <h4>{props.details.name}</h4>
         <p>{props.details.description}</p>
@@ -22,26 +14,7 @@ function SingleProduct(props) {
           Price : $ <strong>{props.details.price}</strong>
         </p>
         <p>In Stock</p>
-        <p>
-          Quantity :{" "}
-          {quantity > 10
-            ? "available"
-            : quantity === 0
-            ? "unavailable"
-            : "selling fast"}
-        </p>
-        <div id="product-variants">
-          <button
-            id="product-variant"
-            style={{ backgroundColor: "grey" }}
-            onClick={() => handleDetails(props.details.variants[0])}
-          ></button>
-          <button
-            id="product-variant"
-            style={{ backgroundColor: "Blue" }}
-            onClick={() => handleDetails(props.details.variants[1])}
-          ></button>
-        </div>
+        <p>Quantity : ${props.details.quantity}</p>
         <button type="button" id="add-to-cart-btn">
           Add to Cart
         </button>
@@ -54,6 +27,7 @@ const Product = () => {
   const { id } = useParams();
   const { data, isLoading, isFetching } = useProductData(id === ":id" ? 1 : id);
   if (isLoading || isFetching) return <div>Loading...</div>;
+  console.log("ID : ", id);
   return (
     <div id="product-page-block">
       <SingleProduct details={data?.data} />
